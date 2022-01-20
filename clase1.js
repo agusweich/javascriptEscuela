8ec812784eec8a8ab314458e2cda4a20cfb5872c
agustin <agusweich@hotmail.com>
2022-01-20 17:22:04 -0300

    primer cambio

---
+++ clase1.js
@@ -0,0 +1,22 @@
+ function Saludar (){
+    alert("Bienvenidos") //Declara función
+}
+
+Saludar(); //llama función
+// Saludar();
+
+
+
+let notaprimera= parseInt (prompt("ingrese nota 1"))
+let notasegunda= parseInt (prompt("ingrese nota 2"))
+let notafinal= (notaprimera + notasegunda)/2
+
+alert ("su nota final es " + notafinal)
+
+if (notafinal >=7){
+    alert ("Aprobado")
+}else{
+    alert ("Desaprobado")
+}
+
+
